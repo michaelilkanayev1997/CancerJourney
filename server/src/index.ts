74e447e8e9b0a0c07c310bc11906df198db1a2a0
vovadavidzon <vovadavidzon30@gmail.com>
2023-12-25 21:59:19 +0200

    added db setup and variables file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import "dotenv/config";
+import "./db";
 
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log("port is listening on port " + PORT);
