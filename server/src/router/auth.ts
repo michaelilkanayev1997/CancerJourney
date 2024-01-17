28303356ead71df83e780fc36e2e5a329a93fd88
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-17 13:29:39 +0200

    Added CreateUser Validation with YUP

--- server/src/router/auth.ts
+++ server/src/router/auth.ts
@@ -1,14 +1,20 @@
 import { CreateUser } from "#/@types/user";
+import { validate } from "#/middleware/validator";
 import User from "#/models/user";
+import { CreateUserSchema } from "#/utils/validationSchema";
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/create", async (req: CreateUser, res) => {
-  const { email, password, name } = req.body;
+router.post(
+  "/create",
+  validate(CreateUserSchema),
+  async (req: CreateUser, res) => {
+    const { email, password, name } = req.body;
 
-  const user = await User.create({ email, password, name });
-  res.json({ user });
-});
+    const user = await User.create({ email, password, name });
+    res.json({ user });
+  }
+);
 
 export default router;
