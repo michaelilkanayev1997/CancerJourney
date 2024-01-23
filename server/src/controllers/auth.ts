deb54ffcfee31e93964f78bebb2a1aab9268ce0b
vovadavidzon <vovadavidzon30@gmail.com>
2024-01-23 16:23:24 +0200

    added email verification token schema

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -2,8 +2,10 @@ import { RequestHandler } from "express";
 
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
+import EmailVerificationToken from "#/models/emailVerificationToken";
 import nodemailer from "nodemailer";
 import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
+import { generateToken } from "#/utils/helper";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
@@ -20,10 +22,16 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     },
   });
 
+  const token = generateToken(6);
+  await EmailVerificationToken.create({
+    owner: user._id,
+    token,
+  });
+
   transport.sendMail({
     to: user.email,
     from: "auth@myapp.com",
-    html: "<h1>123456</h1>",
+    html: `<h1>Your Varefication token is ${token}</h1>`,
   });
 
   res.status(201).json({ user });
