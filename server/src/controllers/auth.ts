88f431c67e3d953be1f32cda1f00a263207d818f
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-25 15:42:55 +0200

    Added Mail Template With Attachment

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import path from "path";
 
 import { CreateUser } from "#/@types/user";
 import User from "#/models/user";
@@ -6,6 +7,7 @@ import EmailVerificationToken from "#/models/emailVerificationToken";
 import nodemailer from "nodemailer";
 import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables";
 import { generateToken } from "#/utils/helper";
+import { generateTemplate } from "#/mail/template";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
@@ -28,10 +30,32 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     token,
   });
 
+  const welcomeMessage = `Hi ${name}, welcome to CancerJourny! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
+
   transport.sendMail({
     to: user.email,
     from: "auth@myapp.com",
-    html: `<h1>Your Varefication token is ${token}</h1>`,
+    subject: "Welcome Message",
+    html: generateTemplate({
+      title: "Welcome to Cancer Journy",
+      message: welcomeMessage,
+      logo: "cid:logo",
+      banner: "cid:welcome",
+      link: "#",
+      btnTitle: token,
+    }),
+    attachments: [
+      {
+        filename: "logo.png",
+        path: path.join(__dirname, "../mail/logo.png"),
+        cid: "logo",
+      },
+      {
+        filename: "welcome.png",
+        path: path.join(__dirname, "../mail/welcome.png"),
+        cid: "welcome",
+      },
+    ],
   });
 
   res.status(201).json({ user });
