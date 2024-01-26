3afe181a35f6d5740460c47084680c181f6f9f38
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-26 21:18:03 +0200

    Verifying Emails

--- server/src/router/auth.ts
+++ server/src/router/auth.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { create } from "#/controllers/auth";
+import { create, verifyEmail } from "#/controllers/auth";
 import { validate } from "#/middleware/validator";
 import { CreateUserSchema } from "#/utils/validationSchema";
 
 const router = Router();
 
 router.post("/create", validate(CreateUserSchema), create);
+router.post("/verify-email", verifyEmail);
 
 export default router;
