1049c0fa3351df98852ed99ba7ff1049010c4e77
vovadavidzon <vovadavidzon30@gmail.com>
2024-01-28 20:28:18 +0200

    added re-verification-email

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -5,6 +5,8 @@ import User from "#/models/user";
 import { generateToken } from "#/utils/helper";
 import { sendVerificationMail } from "#/utils/mail";
 import EmailVerificationToken from "#/models/emailVerificationToken";
+import emailVerificationToken from "#/models/emailVerificationToken";
+import { isValidObjectId } from "mongoose";
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
@@ -43,4 +45,29 @@ export const verifyEmail: RequestHandler = async (
   await EmailVerificationToken.findByIdAndDelete(verificationToken._id);
 
   res.json({ messgae: "Your email is verified!" });
+};
+
+export const sendReVerificationToken: RequestHandler = async (req, res) => {
+  const { userId } = req.body;
+
+  if (!isValidObjectId(userId))
+    return res.status(403).json({ error: "Invalid Request !" });
+
+  const user = await User.findById(userId);
+  if (!user) return res.status(403).json({ error: "Invalid Request !" });
+
+  await EmailVerificationToken.findOneAndDelete({ owner: userId });
+
+  const token = generateToken(4);
+
+  await EmailVerificationToken.create({ owner: userId, token });
+
+  // Send Verification Email
+  sendVerificationMail(token, {
+    name: user.name,
+    email: user.email,
+    userId: user._id.toString(),
+  });
+
+  res.json({ messgae: "please check your email." });
 };
