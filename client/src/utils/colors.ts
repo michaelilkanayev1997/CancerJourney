8c911d39e939a8d3c45ac30b119a8fafeb2cb4cd
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-02-02 00:19:27 +0200

    Added Icons Assets

---
+++ client/src/utils/colors.ts
@@ -0,0 +1,27 @@
+const PRIMARY = "#C5D4E8";
+const PRIMARY_DARK1 = "#A9BFD9";
+const PRIMARY_DARK2 = "#7B8EAB";
+const SECONDARY = "#202444";
+const THIRD = "#26305C";
+const CONTRAST = "#202444";
+const INACTIVE_CONTRAST = "#F4F4F4";
+const ERROR = "#db3125";
+const SUCCESS = "#26A81F";
+const OVERLAY = "rgba(45, 35, 35, 0.5)";
+const Info = "#20639B";
+
+const colors = {
+  PRIMARY,
+  SECONDARY,
+  THIRD,
+  CONTRAST,
+  INACTIVE_CONTRAST,
+  ERROR,
+  SUCCESS,
+  OVERLAY,
+  Info,
+  PRIMARY_DARK1,
+  PRIMARY_DARK2,
+};
+
+export default colors;
