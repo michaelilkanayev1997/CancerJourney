ce48d4d3c10e538de4810f191f1fe1835deecfa7
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-02-03 01:52:40 +0200

    Added Animated Splash Screen

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,48 @@
-import LottieAnimation from "@components/LottieAnimation";
 import SignUp from "@views/auth/SignUp";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import LottieAnimation from "@components/LottieAnimation";
+import Animated from "react-native-reanimated";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [splashAnimationFinished, setSplashAnimationFinished] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const showAnimatedSplash = !appIsReady || !splashAnimationFinished;
+
   return (
-    <View style={styles.container}>
-      <LottieAnimation />
-      <SignUp />
+    <Animated.View style={styles.container}>
+      {showAnimatedSplash ? (
+        <LottieAnimation
+          onAnimationFinish={(isCancelled) => {
+            if (!isCancelled) {
+              setSplashAnimationFinished(true);
+            }
+          }}
+        />
+      ) : (
+        <SignUp />
+      )}
       <StatusBar style="auto" />
-    </View>
+    </Animated.View>
   );
 }
 
