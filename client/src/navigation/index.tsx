fd1f25e2254f8314ba1162913cc32f43aebbd2d2
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-02-19 17:11:14 +0200

    Added Loading Component

--- client/src/navigation/index.tsx
+++ client/src/navigation/index.tsx
@@ -1,27 +1,39 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { FC, useEffect } from "react";
-import AuthNavigator from "./AuthNavigator";
 import { useDispatch, useSelector } from "react-redux";
+
 import {
   getAuthState,
+  updateBusyState,
   updateLoggedInState,
   updateProfile,
 } from "src/store/auth";
+import AuthNavigator from "./AuthNavigator";
 import TabNavigator from "./TabNavigator";
-import { Keys, getFromAsyncStorage } from "@utils/asyncStorage";
+import {
+  Keys,
+  clearAsyncStorage,
+  getFromAsyncStorage,
+} from "@utils/asyncStorage";
 import client from "src/api/client";
+import { StyleSheet, View } from "react-native";
+import Loader from "@ui/Loader";
+import colors from "@utils/colors";
 
 interface Props {}
 
 const AppNavigator: FC<Props> = (props) => {
-  const { loggedIn } = useSelector(getAuthState);
+  const { loggedIn, busy } = useSelector(getAuthState);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchAuthInfo = async () => {
+      dispatch(updateBusyState(true));
       try {
         const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
-        if (!token) return;
+        if (!token) {
+          return dispatch(updateBusyState(false));
+        }
 
         const { data } = await client.get("/auth/is-auth", {
           headers: { Authorization: "Bearer " + token },
@@ -34,6 +46,10 @@ const AppNavigator: FC<Props> = (props) => {
       } catch (error) {
         console.log(error);
       }
+
+      setTimeout(() => {
+        dispatch(updateBusyState(false));
+      }, 4000);
     };
 
     fetchAuthInfo();
@@ -41,9 +57,26 @@ const AppNavigator: FC<Props> = (props) => {
 
   return (
     <NavigationContainer>
+      {busy ? (
+        <View style={styles.loaderContainer}>
+          <Loader />
+        </View>
+      ) : null}
+
       {loggedIn ? <TabNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  loaderContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+    transform: [{ scale: 1.5 }],
+    backgroundColor: colors.PRIMARY_DARK2,
+  },
+});
 
 export default AppNavigator;
