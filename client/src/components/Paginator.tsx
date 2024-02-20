e23f8cecde14ed8758a5897ddda676f1d6e3dab7
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-02-20 22:48:42 +0200

    Added OnBoarding Logic and Improvments

---
+++ client/src/components/Paginator.tsx
@@ -0,0 +1,71 @@
+import { FC } from "react";
+import {
+  View,
+  StyleSheet,
+  useWindowDimensions,
+  Animated,
+  ImageSourcePropType,
+} from "react-native";
+
+import colors from "@utils/colors";
+
+// The structure of each slide's data
+interface SlideData {
+  id: string;
+  title: string;
+  description: string;
+  image: ImageSourcePropType;
+}
+
+interface Props {
+  data: SlideData[];
+  scrollX: Animated.Value;
+}
+
+const Paginator: FC<Props> = ({ data, scrollX }) => {
+  const { width } = useWindowDimensions(); // Get the window's width to calculate the dot's position
+
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {data.map((_, i) => {
+        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+        const scale = scrollX.interpolate({
+          inputRange,
+          outputRange: [1.0, 1.4, 1.0],
+          extrapolate: "clamp",
+        });
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.3, 0.8, 0.3],
+          extrapolate: "clamp",
+        });
+        return (
+          <Animated.View
+            key={i}
+            style={{
+              height: 10,
+              width: 10,
+              borderRadius: 5,
+              backgroundColor: colors.PRIMARY_BTN,
+              margin: 10,
+              opacity,
+              transform: [{ scale }],
+            }}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default Paginator;
--- client/src/views/OnBoarding.tsx
+++ client/src/views/OnBoarding.tsx
@@ -1,65 +1,19 @@
 import { FC, useRef, useState } from "react";
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  Animated,
-  useWindowDimensions,
-} from "react-native";
-import slidesData from "@assets/OnBoarding/slidesData";
+import { View, StyleSheet, FlatList, Animated } from "react-native";
 
+import slidesData from "@assets/OnBoarding/slidesData";
 import OnBoardingItem from "@components/OnBoardingItem";
 import colors from "@utils/colors";
+import Paginator from "@components/Paginator";
+import AppLink from "@ui/AppLink";
 
 interface Props {}
-
-const Paginator = ({ data, scrollX }) => {
-  const { width } = useWindowDimensions(); // Get the window's width to calculate the dot's position
-
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {data.map((_, i) => {
-        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
-        const scale = scrollX.interpolate({
-          inputRange,
-          outputRange: [1.0, 1.4, 1.0],
-          extrapolate: "clamp",
-        });
-        const opacity = scrollX.interpolate({
-          inputRange,
-          outputRange: [0.2, 0.8, 0.2],
-          extrapolate: "clamp",
-        });
-        return (
-          <Animated.View
-            key={i}
-            style={{
-              height: 10,
-              width: 10,
-              borderRadius: 5,
-              backgroundColor: colors.PRIMARY_BTN,
-              margin: 10,
-              opacity,
-              transform: [{ scale }],
-            }}
-          />
-        );
-      })}
-    </View>
-  );
-};
 
 const OnBoarding: FC<Props> = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const scrollX = useRef(new Animated.Value(0)).current;
-  const slidesRef = useRef(null);
+  const slidesRef = useRef<FlatList<any>>(null);
 
   const viewableItemsChanged = useRef(({ viewableItems }) => {
     setCurrentIndex(viewableItems[0].index);
@@ -67,6 +21,18 @@ const OnBoarding: FC<Props> = (props) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.skipButton}>
+        <AppLink
+          style={styles.skipButtonText}
+          title="Skip"
+          onPress={() => {
+            slidesRef.current?.scrollToIndex({
+              index: slidesData.length - 1,
+              animated: true,
+            });
+          }}
+        />
+      </View>
       <View style={{ flex: 3 }}>
         <FlatList
           data={slidesData}
@@ -76,6 +42,7 @@ const OnBoarding: FC<Props> = (props) => {
           pagingEnabled
           bounces={false}
           keyExtractor={(item) => item.id}
+          overScrollMode="never"
           onScroll={Animated.event(
             [{ nativeEvent: { contentOffset: { x: scrollX } } }],
             {
@@ -87,7 +54,7 @@ const OnBoarding: FC<Props> = (props) => {
           ref={slidesRef}
         />
       </View>
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 0.5 }}>
         <Paginator data={slidesData} scrollX={scrollX} />
       </View>
     </View>
@@ -99,6 +66,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: colors.PRIMARY_DARK2,
+  },
+  skipButton: {
+    position: "absolute",
+    top: 40,
+    right: 20,
+    padding: 10,
+    zIndex: 100,
+  },
+  skipButtonText: {
+    color: colors.PRIMARY_BTN,
+    fontSize: 16,
   },
 });
 
