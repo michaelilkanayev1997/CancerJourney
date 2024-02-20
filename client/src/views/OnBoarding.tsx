696ca3375a8127473f6592295dde005b60dfaaf2
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-02-20 21:36:02 +0200

    Added Paginator For OnBoarding

--- client/src/views/OnBoarding.tsx
+++ client/src/views/OnBoarding.tsx
@@ -1,9 +1,59 @@
 import { FC, useRef, useState } from "react";
-import { View, StyleSheet, Text, FlatList, Animated } from "react-native";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Animated,
+  useWindowDimensions,
+} from "react-native";
 import slidesData from "@assets/OnBoarding/slidesData";
 
 import OnBoardingItem from "@components/OnBoardingItem";
+import colors from "@utils/colors";
+
 interface Props {}
+
+const Paginator = ({ data, scrollX }) => {
+  const { width } = useWindowDimensions(); // Get the window's width to calculate the dot's position
+
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {data.map((_, i) => {
+        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+        const scale = scrollX.interpolate({
+          inputRange,
+          outputRange: [1.0, 1.4, 1.0],
+          extrapolate: "clamp",
+        });
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.2, 0.8, 0.2],
+          extrapolate: "clamp",
+        });
+        return (
+          <Animated.View
+            key={i}
+            style={{
+              height: 10,
+              width: 10,
+              borderRadius: 5,
+              backgroundColor: colors.PRIMARY_BTN,
+              margin: 10,
+              opacity,
+              transform: [{ scale }],
+            }}
+          />
+        );
+      })}
+    </View>
+  );
+};
 
 const OnBoarding: FC<Props> = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -22,6 +72,7 @@ const OnBoarding: FC<Props> = (props) => {
           data={slidesData}
           renderItem={({ item }) => <OnBoardingItem item={item} />}
           horizontal
+          showsHorizontalScrollIndicator={false}
           pagingEnabled
           bounces={false}
           keyExtractor={(item) => item.id}
@@ -35,6 +86,9 @@ const OnBoarding: FC<Props> = (props) => {
           onViewableItemsChanged={viewableItemsChanged}
           ref={slidesRef}
         />
+      </View>
+      <View style={{ flex: 1 }}>
+        <Paginator data={slidesData} scrollX={scrollX} />
       </View>
     </View>
   );
