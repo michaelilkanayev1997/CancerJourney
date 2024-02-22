b83b2ac4b94cbeb49c567de2639c85a339eb9bc9
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-02-23 01:33:51 +0200

    Animations Refactor with Custom Hooks

--- client/src/utils/asyncStorage.ts
+++ client/src/utils/asyncStorage.ts
@@ -18,6 +18,5 @@ export const clearAsyncStorage = async () => {
 
 export enum Keys {
   AUTH_TOKEN = "AUTH_TOKEN",
-  GOOGLE_AUTH_TOKEN = "GOOGLE_AUTH_TOKEN",
   VIEWED_ON_BOARDING = "@viewedOnBoarding",
 }
