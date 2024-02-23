d1b8621ba7d61d8f6590d0b9c3d105d0699f9bd2
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-02-24 00:43:58 +0200

    BottomTab Code Refactor

--- client/src/navigation/TabNavigator.tsx
+++ client/src/navigation/TabNavigator.tsx
@@ -1,134 +1,13 @@
-import { FC, ReactNode, useCallback, useState } from "react";
-import { Feather } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {
-  GestureResponderEvent,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { Image, StyleSheet } from "react-native";
 
-import colors from "@utils/colors";
 import Home from "@views/bottonTab/Home";
 import Profile from "@views/bottonTab/Profile";
 import Upload from "@views/bottonTab/Upload";
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from "react-native-reanimated";
-import { useFocusEffect } from "@react-navigation/native";
+import CustomPostButton from "@ui/bottomTab/CustomPostButton";
+import AnimatedIcon from "@ui/bottomTab/AnimatedIcon";
 
 const Tab = createBottomTabNavigator();
-
-interface CustomPostTabBarButtonProps {
-  children: ReactNode;
-  onPress?: (event: GestureResponderEvent | any) => void;
-}
-
-const CustomPostTabBarButton: FC<CustomPostTabBarButtonProps> = ({
-  children,
-  onPress,
-}) => {
-  const backgroundColor = useSharedValue("white");
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      backgroundColor: backgroundColor.value,
-    };
-  });
-
-  useFocusEffect(
-    useCallback(() => {
-      // Animate background color when focused
-      backgroundColor.value = withTiming(colors.PRIMARY_DARK2, {
-        duration: 500,
-      });
-      return () => {
-        // Reset background color when losing focus
-        backgroundColor.value = withTiming("white", {
-          duration: 600,
-        });
-      };
-    }, [])
-  );
-
-  return (
-    <View style={{ top: -30, justifyContent: "center", alignItems: "center" }}>
-      <Animated.View
-        style={[
-          {
-            width: 70,
-            height: 70,
-            borderRadius: 35,
-            ...styles.shadow,
-          },
-          animatedStyle,
-        ]}
-      >
-        <TouchableOpacity
-          onPress={onPress}
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          {children}
-        </TouchableOpacity>
-      </Animated.View>
-    </View>
-  );
-};
-
-const AnimatedIcon = ({ label, icon, size, color }) => {
-  const translateY = useSharedValue(0);
-  const scale = useSharedValue(1);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateY: translateY.value }, { scale: scale.value }],
-    };
-  });
-
-  useFocusEffect(
-    useCallback(() => {
-      translateY.value = withTiming(-15, {
-        duration: 500,
-      });
-      scale.value = withTiming(1.1, {
-        duration: 500,
-      });
-
-      return () => {
-        // Cleanup function to execute when component loses focus
-        translateY.value = withTiming(0, {
-          duration: 500,
-        });
-        scale.value = withTiming(1, {
-          duration: 500,
-        });
-      };
-    }, [])
-  );
-
-  return (
-    <Animated.View
-      style={[
-        { alignItems: "center", justifyContent: "center", top: 10 },
-        animatedStyle,
-      ]}
-    >
-      <Feather name={icon} size={size} color={color} />
-      <Text
-        style={{
-          color: color,
-          fontSize: 12,
-        }}
-      >
-        {label}
-      </Text>
-    </Animated.View>
-  );
-};
 
 const TabNavigator = () => {
   return (
@@ -137,14 +16,7 @@ const TabNavigator = () => {
         headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: {
-          backgroundColor: "white",
-          position: "absolute",
-          bottom: 5,
-          left: 5,
-          right: 5,
-          borderRadius: 15,
-          height: 80,
-          ...styles.shadow,
+          ...styles.tabBarStyle,
         },
       }}
     >
@@ -185,7 +57,7 @@ const TabNavigator = () => {
             );
           },
 
-          tabBarButton: (props) => <CustomPostTabBarButton {...props} />,
+          tabBarButton: (props) => <CustomPostButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -221,7 +93,14 @@ const TabNavigator = () => {
 };
 
 const styles = StyleSheet.create({
-  shadow: {
+  tabBarStyle: {
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: 5,
+    left: 5,
+    right: 5,
+    borderRadius: 15,
+    height: 80,
     shadowColor: "black",
     shadowOffset: {
       width: 0,
