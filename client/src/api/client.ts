9b8fc5ff569cae87a808260643d302b31d5898ec
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-03-02 23:34:26 +0200

    Added Logout Option

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,25 @@
+import { Keys, getFromAsyncStorage } from "@utils/asyncStorage";
 import axios, { CreateAxiosDefaults } from "axios";
 
+const baseURL = "http://10.0.0.9:8000";
+
 const client = axios.create({
-  baseURL: "http://10.0.0.9:8000",
+  baseURL,
 });
+
+type headers = CreateAxiosDefaults<any>["headers"];
+
+export const getClient = async (headers?: headers) => {
+  const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
+
+  if (!token) return axios.create({ baseURL });
+
+  const defaultHeaders = {
+    Authorization: "Bearer " + token,
+    ...headers,
+  };
+
+  return axios.create({ baseURL, headers: defaultHeaders });
+};
 
 export default client;
--- client/src/views/bottomTab/Settings.tsx
+++ client/src/views/bottomTab/Settings.tsx
@@ -1,19 +1,148 @@
+import colors from "@utils/colors";
 import { FC } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAuthState,
+  updateBusyState,
+  updateLoggedInState,
+  updateProfile,
+} from "src/store/auth";
+import { Keys, removeFromAsyncStorage } from "@utils/asyncStorage";
+import catchAsyncError from "src/api/catchError";
+import client, { getClient } from "src/api/client";
+import { ToastNotification } from "@utils/toastConfig";
 
 interface Props {}
 
 const Settings: FC<Props> = (props) => {
+  const dispatch = useDispatch();
+  const { profile } = useSelector(getAuthState);
+
+  const handleLoggout = async (fromAll?: boolean) => {
+    dispatch(updateBusyState(true));
+    try {
+      const endpoint = "/auth/log-out?fromAll=" + (fromAll ? "yes" : "");
+
+      const client = await getClient();
+      await client.post(endpoint);
+      await removeFromAsyncStorage(Keys.AUTH_TOKEN);
+      dispatch(updateProfile(null));
+      dispatch(updateLoggedInState(false));
+    } catch (error) {
+      const errorMessage = catchAsyncError(error);
+      ToastNotification({
+        type: "Error",
+        message: errorMessage,
+      });
+    }
+    dispatch(updateBusyState(false));
+  };
   return (
     <View style={styles.container}>
-      <Text>Settings 🎉</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Profile Settings</Text>
+      </View>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>History</Text>
+      </View>
+
+      <View style={styles.settingOptionsContainer}>
+        <Pressable
+          onPress={() => console.log("first")}
+          style={styles.buttonContainer}
+        >
+          <Text style={styles.buttonTitle}>Clear All</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Logout</Text>
+      </View>
+
+      <View style={styles.settingOptionsContainer}>
+        <Pressable
+          onPress={() => handleLoggout(true)}
+          style={styles.buttonContainer}
+        >
+          <AntDesign name="logout" size={20} color={colors.CONTRAST} />
+          <Text style={styles.buttonTitle}>Logout From All</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => handleLoggout()}
+          style={styles.buttonContainer}
+        >
+          <AntDesign name="logout" size={20} color={colors.CONTRAST} />
+          <Text style={styles.buttonTitle}>Logout</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding: 10,
+  },
+  titleContainer: {
+    borderBottomWidth: 0.5,
+    borderBottomColor: colors.SECONDARY,
+    paddingBottom: 5,
+    marginTop: 15,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: colors.SECONDARY,
+  },
+  settingOptionsContainer: {
+    marginTop: 15,
+    paddingLeft: 15,
+  },
+  avatarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  linkText: {
+    color: colors.SECONDARY,
+    fontStyle: "italic",
+  },
+  paddingLeft: {
+    paddingLeft: 15,
+  },
+  nameInput: {
+    color: colors.CONTRAST,
+    fontWeight: "bold",
+    fontSize: 18,
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: colors.CONTRAST,
+    borderRadius: 7,
+    marginTop: 15,
+  },
+  emailConainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  email: {
+    color: colors.CONTRAST,
+    marginRight: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  buttonTitle: {
+    color: colors.CONTRAST,
+    fontSize: 18,
+    marginLeft: 5,
+  },
+  marginTop: {
+    marginTop: 15,
   },
 });
 
