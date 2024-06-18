4600c2e2efb12eebd34a357f2ac63e2920614657
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-06-18 13:19:15 +0300

    Added add-medication router & controller & validation

--- server/src/models/Schedule.ts
+++ server/src/models/Schedule.ts
@@ -8,10 +8,20 @@ export interface IAppointment extends Document {
   notes?: string;
 }
 
+export interface IMedication extends Document {
+  _id: ObjectId;
+  name: string;
+  frequency: string;
+  timesPerDay?: string;
+  specificDays?: string;
+  prescriber?: string;
+  notes?: string;
+}
+
 interface IUserSchedule extends Document {
   owner: ObjectId;
-  bloodtests: IAppointment[];
-  treatments: IAppointment[];
+  appointments: IAppointment[];
+  medications: IAppointment[];
 }
 
 const AppointmentSchema: Schema = new Schema({
@@ -21,10 +31,19 @@ const AppointmentSchema: Schema = new Schema({
   notes: { type: String },
 });
 
+const MedicationSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  frequency: { type: String },
+  timesPerDay: { type: String },
+  specificDays: { type: String },
+  prescriber: { type: String },
+  notes: { type: String },
+});
+
 const UserScheduleSchema: Schema = new Schema({
   owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
   appointments: [AppointmentSchema],
-  medications: [AppointmentSchema],
+  medications: [MedicationSchema],
 });
 
 export const Schedule = mongoose.model<IUserSchedule>(
--- server/src/router/schedule.ts
+++ server/src/router/schedule.ts
@@ -2,10 +2,22 @@ import { Router } from "express";
 
 import { mustAuth } from "#/middleware/auth";
 import { validate } from "#/middleware/validator";
-import { addAppointment } from "#/controllers/schedule";
+import { addAppointment, addMedication } from "#/controllers/schedule";
+import { AppointmentSchema, MedicationSchema } from "#/utils/validationSchema";
 
 const schedule = Router();
 
-schedule.post("/add-appointment", mustAuth, addAppointment);
+schedule.post(
+  "/add-appointment",
+  mustAuth,
+  validate(AppointmentSchema),
+  addAppointment
+);
+schedule.post(
+  "/add-medication",
+  mustAuth,
+  validate(MedicationSchema),
+  addMedication
+);
 
 export default schedule;
