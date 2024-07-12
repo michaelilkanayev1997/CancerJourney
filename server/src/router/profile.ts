a2a437da5a6399653a313f871d2a3f04a53400b4
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-07-13 01:20:49 +0300

    Added getFollowers & getFollowings Controllers

--- server/src/router/profile.ts
+++ server/src/router/profile.ts
@@ -1,8 +1,16 @@
-import { mustAuth } from "#/middleware/auth";
 import { Router } from "express";
+
+import {
+  getFollowers,
+  getFollowings,
+  updateFollower,
+} from "#/controllers/profile";
+import { mustAuth } from "#/middleware/auth";
 
 const router = Router();
 
 router.post("/update-follower/:profileId", mustAuth, updateFollower);
+router.get("/get-followers/:profileId", mustAuth, getFollowers);
+router.get("/get-followings/:profileId", mustAuth, getFollowings);
 
 export default router;
