0579e606ef8d8e8678ace1e6a38989d8709e3711
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-07-13 00:36:48 +0300

    Added profile Router + updateFollower Controller

---
+++ server/src/controllers/profile.ts
@@ -0,0 +1,65 @@
+import { RequestHandler } from "express";
+import { isValidObjectId } from "mongoose";
+
+import User from "#/models/user";
+
+export const updateFollower: RequestHandler = async (req, res) => {
+  const { profileId } = req.params;
+  let status: "added" | "removed";
+
+  // Check if profileId is a valid object id OR profileId is now the authenticated user
+  if (!isValidObjectId(profileId) || profileId === req.user.id.toString())
+    return res.status(422).json({ error: "Invalid profile id!" });
+
+  const profile = await User.findById(profileId);
+  if (!profile) return res.status(404).json({ error: "Profile not found!" });
+
+  const alreadyAFollower = await User.findOne({
+    _id: profileId,
+    followers: req.user.id,
+  });
+
+  if (alreadyAFollower) {
+    // un follow
+    await User.updateOne(
+      {
+        _id: profileId,
+      },
+      {
+        $pull: { followers: req.user.id },
+      }
+    );
+
+    status = "removed";
+  } else {
+    // follow the user
+    await User.updateOne(
+      {
+        _id: profileId,
+      },
+      {
+        $addToSet: { followers: req.user.id },
+      }
+    );
+
+    status = "added";
+  }
+
+  if (status === "added") {
+    // update the following list (add)
+    await User.updateOne(
+      { _id: req.user.id },
+      { $addToSet: { followings: profileId } }
+    );
+  }
+
+  if (status === "removed") {
+    // remove from the following list (remove)
+    await User.updateOne(
+      { _id: req.user.id },
+      { $pull: { followings: profileId } }
+    );
+  }
+
+  res.json({ status });
+};
---
+++ server/src/router/profile.ts
@@ -0,0 +1,8 @@
+import { mustAuth } from "#/middleware/auth";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/update-follower/:profileId", mustAuth, updateFollower);
+
+export default router;
